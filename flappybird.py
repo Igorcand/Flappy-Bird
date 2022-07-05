9b4f8752ae4ea8812d72138927fa713db343cc41
Igor Candido Rodrigues <igorcr39@gmail.com>
2022-07-05 07:15:15 -0300

    update name folder

--- flappybird.py
+++ flappybird.py
@@ -9,13 +9,13 @@ geracao = 0
 TELA_LARGURA = 500
 TELA_ALTURA = 800
 
-IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
-IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
-IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bg.png')))
 IMAGENS_PASSARO = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'bird3.png'))),
 ]
 
 pygame.font.init()
